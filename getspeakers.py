43b28a56fe886d13e62c6af98d588182459cdbf8
Battle420 <battlenationyt@gmail.com>
2023-01-03 16:28:51 +0100

    extract portrait from each speaker and their json (more info to be extracted from the json later)

--- getspeakers.py
+++ getspeakers.py
@@ -1,15 +1,17 @@
 import requests
 import json
+import base64
+import os
 
 try:
     response = requests.get('http://localhost:50021/speakers')
 except requests.ConnectionError:
     print("No connection")
     exit()
-    if response.status_code == 200 and 'application/json' \
+if response.status_code == 200 and 'application/json' \
             in response.headers.get('Content-Type', ''):
-        with open('speakers.json', 'bw') as f:
-            f.write(response.content)
+    with open('speakers.json', 'bw') as f:
+        f.write(response.content)
 
 
 with open('speakers.json') as j:
@@ -28,7 +30,18 @@ for i in speakeruuid:
         print("No connection")
         exit()
 
-        if response.status_code == 200 and 'application/json' \
-                in response.headers.get('Content-Type', ''):
-            with open('speakers/{}.json'.format(i), 'bw') as f:
-                f.write(response.content)
+    if response.status_code == 200 and 'application/json' \
+            in response.headers.get('Content-Type', ''):
+        path = 'speakers/{}'.format(i)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        with open('speakers/{}/{}.json'.format(i, i), 'bw') as f:
+            f.write(response.content)
+
+        with open('speakers/{}/{}.json'.format(i, i)) as j:
+            decodedj = json.load(j)
+
+        image_binary = base64.b64decode(decodedj["portrait"])
+        with open('speakers/{}/{}.png'.format(i, i), 'wb') as f:
+            f.write(image_binary)
