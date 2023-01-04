c1f5864e46e33c12737e892093b4cd1529bb0f5f
Battle420 <battlenationyt@gmail.com>
2023-01-04 16:16:50 +0100

    extract all samples and icons done :)

--- getspeakers.py
+++ getspeakers.py
@@ -46,3 +46,28 @@ for i in speakeruuid:
         image_binary = base64.b64decode(decodedj["portrait"])
         with open('speakers/{}/{}.png'.format(i, i), 'wb') as f:
             f.write(image_binary)
+
+        styleids = decodedj['style_infos']
+
+        index = -1
+        for x in styleids:
+            index = index + 1
+            icon_binary = base64.b64decode(x['icon'])
+            styleindex = decodedj['style_infos'][index]['id']
+            path = 'speakers/{}/styles/{}'.format(i, styleindex)
+
+            if not os.path.exists(path):
+                os.makedirs(path)
+
+            with open('{}/{}.png'.format(path, styleindex), 'bw') as f:
+                f.write(icon_binary)
+
+            samples = (decodedj['style_infos'][index]['voice_samples'])
+            sampleindex = -1
+            for y in samples:
+                sampleindex = sampleindex + 1
+                samplebinary = base64.b64decode(samples[sampleindex])
+
+                with open('{}/sample{}.wav'.format(path, sampleindex), 'bw') \
+                        as f:
+                    f.write(samplebinary)
